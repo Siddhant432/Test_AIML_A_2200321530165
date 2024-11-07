467557a4118788a2a64feb5630073c3e9760e227
Siddhant Gogia <sidgogia20@gmail.com>
2024-11-07 15:13:31 +0530

    added files

---
+++ JS/login.js
@@ -0,0 +1,10 @@
+form=document.getElementById("form")
+userId=document.getElementById("userId")
+password=document.getElementById("password")
+
+form(){
+    if(text.innerHTML!="Admin" && password.innerHTML!="Admin"){
+        prompt("invalid credentials")
+    }
+   
+}
